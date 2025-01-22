6c57781a0d33abc1f8ef49c997e3d390dee7a8d8
chaeyeon-jung <coduschung@naver.com>
2025-01-22 17:09:24 +0900

    Design: modify home background

--- src/components/sidebar/HomeButton.jsx
+++ src/components/sidebar/HomeButton.jsx
@@ -6,18 +6,6 @@ export const ClickedHome = styled.div`
   align-items: center;
   cursor: pointer;
   justify-content: center;
-  width: 45px;
-  height: 45px;
-  border-radius: 5px;
-
-  background-color: ${({ isActive }) => 
-    isActive ? "#ECECEC" : "transparent"};
-  transition: background-color 0.2s ease-in-out;
-
-  position: absolute;
-  top: 75px;
-  left: 15px;
-  z-index: -1000; 
 `
 const HomeButton = ({ isHomeOpen, toggleHome }) => {
     return (
