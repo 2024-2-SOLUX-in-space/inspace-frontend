d519a08bbdd2495d7de52c3c86a29eb09ff22c0f
YeBin0711 <ayb0711@sookmyung.ac.kr>
2025-01-28 05:59:26 +0900

    feat: 공간 목록 불러오는 api 함수 구현

---
+++ src/api/space.js
@@ -0,0 +1,19 @@
+// src/api/space.js
+import api from './api';
+
+export const fetchSpaces = async () => {
+  try {
+    const response = await api.get('/api/spaces');
+    console.log('API Response:', response.data);
+    return response.data.map((space) => ({
+      id: space.spaceId,
+      title: space.sname,
+      coverType: space.sthumb,
+      isPrimary: space.isPrimary,
+      isPublic: space.isPublic,
+    }));
+  } catch (error) {
+    console.error('Error fetching spaces:', error);
+    return [];
+  }
+};
